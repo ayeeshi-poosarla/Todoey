9e7f597323cd5241e565d4fa3f55d6a1fd15a461
Ayeeshi Poosarla <Ayeeshi@macintoshusers-macbook-2.local>
2018-08-06 12:06:02 -0700

    Convert MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ayeeshi Poosarla on 8/6/18.
+//  Copyright © 2018 Ayeeshi Poosarla. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
